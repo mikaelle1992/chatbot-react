a66744e0d44620e0fe3c6f4a830e367fa81f91e6
mikaelle1992 <mikaelle.rubiaps02@gmail.com>
2021-08-03 10:14:30 -0300

    feature: css da pagina

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'reactstrap';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import ChatBot from './components/Chatbot';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button color="danger">Danger!</Button>
-      </header>
+      <Header/>
+      <div className = "conteudo">
+         <Home/>
+      <ChatBot/>
+      </div>
      
+     <Footer/>
     </div>
   );
 }
